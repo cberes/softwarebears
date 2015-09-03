f4cbd98255bd88480c2169b2658426d35002c009
Corey Beres <corey.beres@gmail.com>
2015-09-03 01:08:09 -0400

    portfolio.

--- src/softwarebears/handler.clj
+++ src/softwarebears/handler.clj
@@ -3,6 +3,7 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [softwarebears.routes.home :refer [home-routes]]
+            [softwarebears.routes.portfolio :refer [portfolio-routes]]
             [softwarebears.routes.error :refer [error-routes]]))
 
 (defroutes app-routes
@@ -12,6 +13,7 @@
   (wrap-defaults
     (routes
       home-routes
+      portfolio-routes
       app-routes
       error-routes)
     site-defaults))
