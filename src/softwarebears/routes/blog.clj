fd242b614aff57915364254a7ee99b3384c86f0b
Corey Beres <corey.beres@gmail.com>
2016-12-30 01:54:24 -0500

    refactor some blog code

--- src/softwarebears/routes/blog.clj
+++ src/softwarebears/routes/blog.clj
@@ -8,46 +8,57 @@
 
 (def blog-path (System/getenv "SB_BLOG"))
 
+(def markdown-file?
+  (every-pred #(not (.isDirectory %))
+              #(not= (first (.getName %)) \.)
+              #(.endsWith (.getName %) ".md")))
+
 (defn- get-file [item-name]
   (io/file (str blog-path "/" item-name ".md")))
 
-(defn blog-item [markup]
+(defn- blog-item [markup]
   (layout/common
     [:div.intro.full.black
       [:p.middle [:span.name [:span "Software"] [:span "Bears"] ".com"]]]
     [:main.blog
       [:section#blog-item.white markup]]))
 
-(defn get-blog-items []
-  (->>
-    (seq (.listFiles (io/file blog-path)))
-    (filter
-      (apply every-pred [#(not (.isDirectory %))
-                         #(not= (first (.getName %)) \.)
-                         #(.endsWith (.getName %) ".md")]))
+(defn- get-blog-items []
+  (->> blog-path
+    (io/file)
+    (.listFiles)
+    (seq)
+    (filter markdown-file?)
     (sort)
     (reverse)))
 
-(defn read-more-link [f]
-  (let [n (.getName f)]
-    (link-to (str "/blog/" (subs n 0 (- (count n) 3))) "Read more &gt;&gt;&gt;")))
+(defn- trim-end [s suffix]
+  (subs s 0 (- (count s) (count suffix))))
+
+(defn- read-more-link [f]
+  (let [file-name (.getName f)]
+    (link-to (str "/blog/" (trim-end file-name ".md")) "Read more &gt;&gt;&gt;")))
+
+(defn- preview-single-blog-item [f & [prefix]]
+  [(md/preview f prefix) [:h4 (read-more-link f)]])
+
+(defn latest-blog-item []
+  ; hiccup needs this function to return a seq (not a vector)
+  (seq (preview-single-blog-item (first (get-blog-items)) "#")))
+
+(defn- preview-blog-item [f & [prefix]]
+  [(into [:section.white] (preview-single-blog-item f prefix))
+   [:div.divider.trnp.plax.plax4]])
 
 (defn blog []
   (layout/common
     [:div.intro.headline.full.trnp.plax.plax5
       [:p "Writing about technology from an engineer's point of view."]]
     [:main.blog
-      ; the map call returns a lazy sequence of sequences of elements
-      ; so reduce to a single sequence with concat
-      ; then drop the final element, which will be a divider
-      (drop-last
-        (reduce
-          concat
-          (map #(seq [[:section.white (md/preview %) [:h4 (read-more-link %)]] [:div.divider.trnp.plax.plax4]]) (get-blog-items))))]))
-
-(defn latest-blog-item []
-  (let [f (first (get-blog-items))]
-    (seq [(md/preview f "#") [:h4 (read-more-link f)]])))
+      (->> (get-blog-items)
+        (map preview-blog-item)
+        (reduce concat) ; reduce to a single sequence
+        (drop-last))])) ; drop the last element, which is a divider
 
 (defroutes blog-routes
   (GET "/blog/:f" [f] (blog-item (md/render (get-file f))))
