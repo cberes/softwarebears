8dd4107aeb5f32222645c02ffbd783c3b39ba09d
Corey Beres <corey.beres@gmail.com>
2017-02-14 21:23:24 -0500

    new blog entry

--- src/softwarebears/util/markdown.clj
+++ src/softwarebears/util/markdown.clj
@@ -1,14 +1,37 @@
 (ns softwarebears.util.markdown
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [markdown.core :refer [md-to-html-string]]))
 
+(defn- parse-meta [s]
+  (let [[k v] (string/split s #"=" 2)]
+    {(keyword k) v}))
+
+(defn- remove-metadata [s]
+  (string/replace s #"(?s)%%begin-meta%%.*%%end-meta%%" ""))
+
+(defn- get-preview-or-whole [preview? s]
+  (string/replace s (if preview? #"(?s)%%read-more%%.*$" "%%read-more%%") ""))
+
 (defn- read-file [f prefix preview?]
-  (clojure.string/replace
-    (str (or prefix "") (slurp f))
-    (if preview? #"(?s)%%read_more%%.*$" "%%read_more%%") ""))
+  (->> f
+    (slurp)
+    (str (or prefix ""))
+    (remove-metadata)
+    (get-preview-or-whole preview?)))
 
 (defn render [f & [prefix]]
   (md-to-html-string (read-file f prefix false)))
 
 (defn preview [f & [prefix]]
   (md-to-html-string (read-file f prefix true)))
+
+(defn metadata [f]
+  (->> f
+    (slurp)
+    (re-find #"(?s)%%begin-meta%%(.*)%%end-meta%%")
+    (second)
+    (string/split-lines)
+    (map parse-meta)
+    (apply merge)))
+
